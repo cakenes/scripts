d872504f750f9a24cfed45572db64171f501e1d4
Jyri Kurtakko <jyri.kurtakko@gmail.com>
2020-09-15 11:27:02 +0300

    installer.sh updates

--- installer.sh
+++ installer.sh
@@ -5,3 +5,7 @@ sudo pacman -S git
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+cd ..
+rm -fr yay/
+
+yay -S discord blender gimp inkscape krita lollypop lutris obs-studio picom deluge-gtk gameconqueror shotcut vlc
