f35afcebff84d51542f8f0eafd259470c841a6ca
cakenes <jyri.kurtakko@gmail.com>
2020-09-18 20:22:32 +0300

    macro updates

--- wm.sh
+++ wm.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-machine="win10"
+vm="win10"
 
 qemu=$(pidof -x qemu-system-x86_64)
 if [ -z $qemu ]; then
-  virsh --connect qemu:///system start $machine
+  virsh --connect qemu:///system start $vm
 fi
 
-virt-viewer --connect qemu:///system $machine
+virt-viewer --connect qemu:///system $vm
 
 while :
 do
   sleep 1
   viewer=$(pidof -x virt-viewer)
   if [ $viewer >= 0 ]; then
-    virsh --connect qemu:///system shutdown $machine
+    virsh --connect qemu:///system shutdown $vm
     exit 1
   fi
 done
