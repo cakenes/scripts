557b520cf29115b80e96da36ef58a9fe2e7a5e76
Jyri Kurtakko <jyri.kurtakko@gmail.com>
2020-09-25 23:38:14 +0300

    installer script rewrite

--- installer.sh
+++ installer.sh
@@ -1,49 +1,17 @@
 #!/bin/bash
 
-#
-#
-#
-#
-#
-#
-#
-#
+# programs - alacritty balena-etcher blender discord galculator gimp inkscape krita libreoffice-fresh lollypop lutris obs-studio simple-scan steam timeshift visual-studio-code-bin virt-manager virt-viewer vlc yuzu-mainline-bin
+# vulkan nvidia - nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
+# vulkan intel - lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
+# wine - wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader  lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse
+# dev - dotnet-host dotnet-runtime dotnet-sdk dotnet-targeting-pack
+# misc - bash-completion picom rofi dmenu-height htop xss-lock betterlockscreen polybar i3-gaps feh nerd-fonts-complete libinput-gestures grsync intel-undervolt i8kutils xdotool wmctrl kid3-qt easytag qemu hid-nintendo-dkms dell-bios-fan-control-git ebtables libmicrodns protobuf cups sane-airscan
 
 sudo pacman -S yay
 
-# bash
-yay -S bash-completion
+yay -S bash-completion alacritty balena-etcher blender discord galculator gimp inkscape krita lollypop lutris neofetch obs-studio steam visual-studio-code-bin vlc yuzu-mainline-bin libreoffice-fresh virt-viewer virt-manager picom rofi dmenu-height neofetch htop xss-lock betterlockscreen polybar i3-gaps feh nerd-fonts-complete libinput-gestures grsync intel-undervolt i8kutils timeshift xdotool wmctrl kid3-qt easytag qemu hid-nintendo-dkms dell-bios-fan-control-git ebtables libmicrodns protobuf dotnet-host dotnet-runtime dotnet-sdk dotnet-targeting-pack wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
 
-# programs
-yay -S alacritty balena-etcher blender discord galculator gimp inkscape krita lollypop lutris neofetch obs-studio steam visual-studio-code-bin vlc yuzu-mainline-bin libreoffice-fresh virt-viewer virt-manager
-
-# graphical
-yay -S picom rofi dmenu-height neofetch htop xss-lock betterlockscreen polybar i3-gaps feh nerd-fonts-complete libinput-gestures grsync
-
-# misc
-yay -S intel-undervolt i8kutils timeshift xdotool wmctrl kid3-qt easytag qemu hid-nintendo-dkms dell-bios-fan-control-git ebtables
-
-# chromecast
-yay -S libmicrodns protobuf
-
-# dotnet
-yay -S dotnet-host dotnet-runtime dotnet-sdk dotnet-targeting-pack
-
-# print / scan
-yay -S cups sane-airscan simple-scan
-
-# wine
-yay -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader
-
-# vulkan
-#yay -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader
-yay -S lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader
-
-# battle.net
-yay -S lib32-gnutls lib32-libldap lib32-libgpg-error lib32-sqlite lib32-libpulse
-
-# xfce remove
-sudo pacman -Rscn midori lightdm light-locker
+yay -R midori lightdm light-locker
 
 # services
 sudo systemctl enable sshd && sudo systemctl start sshd
--- mage-dps.sh
+++ mage-dps.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#focus=$(xdotool getwindowfocus getwindowname)
-#if [[ $focus != "World of Warcraft" ]]; then
-#  exit 1
-#fi
+focus=$(xdotool getwindowfocus getwindowname)
+if [[ $focus != "World of Warcraft" ]]; then
+  exit 1
+fi
 
 id=$(wmctrl -l | grep "World of Warcraft" | awk '{print $1}')
 color=$(grabc -w $id -l +803+658)
-
-sleep 0.1
 
 if [ $color = "#0000ff" ]; then
   xdotool key "1"
