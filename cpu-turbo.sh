47b4ce97abef0d6c4c5bc93ebc188cde14bfa4f4
cakenes <jyri.kurtakko@gmail.com>
2020-10-24 21:22:49 +0300

    cpu turbo script

---
+++ cpu-turbo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+current=$(cat /sys/devices/system/cpu/intel_pstate/no_turbo)
+if [[ $current == "0" ]]; then
+   notify-send -t 2000 "Turbo Disabled"
+   echo 1 | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/no_turbo
+elif [[ $current == "1" ]]; then
+   notify-send -t 2000 "Turbo Enabled"
+   echo 0 | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/no_turbo
+fi
