865a926f0023e4a38b876c685fa8599bc08b881c
cakenes <jyri.kurtakko@gmail.com>
2020-11-18 12:59:44 +0200

    added max cpu perf script

---
+++ cpu-perf.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+current=$(cat /sys/devices/system/cpu/intel_pstate/max_perf_pct)
+
+if [[ $current == "100" ]]; then
+   notify-send -t 2000 "Max performance: 80%"
+   echo "80" | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+elif [[ $current == "80" ]]; then
+   notify-send -t 2000 "Max performance: 60%"
+   echo "60" | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+elif [[ $current == "60" ]]; then
+   notify-send -t 2000 "Max performance: 40%"
+   echo "40" | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+elif [[ $current == "40" ]]; then
+   notify-send -t 2000 "Max perfromance: 20%"
+   echo "20" | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+else
+   notify-send -t 2000 "Max perfromance: 100%"
+   echo "100" | ~/.config/scripts/tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+fi
