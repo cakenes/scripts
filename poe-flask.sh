264011f26e29718e7d5881ad7ef668cd2329491d
cakenes <jyri.kurtakko@gmail.com>
2020-11-21 21:52:00 +0200

    poe-flask timer mod

--- poe-flask.sh
+++ poe-flask.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-timer=4.3
+timer=3.9
 
 for pid in $(pidof -x poe-flask.sh); do
   if [ $pid != $$ ]; then
+    notify-send -t 2000 "Stopped"
     killall poe-flask.sh
     exit 1
   fi
 done
 
+notify-send -t 2000 "Running"
+
 while :
 do
-  sleep 0.5
+
+  sleeprandom=$(printf "0.2%01d\n" $(( RANDOM % 10 )))
+  keyrandom=$(printf "0.05%01d\n" $(( RANDOM % 10 )))
+
+  sleep $sleeprandom
+
   focus=$(xdotool getwindowfocus getwindowname)
   if [[ $focus != "Path of Exile" ]]; then
     continue
   fi
-#  xdotool key "1"
-  xdotool key "2"
-  xdotool key "3"
-  xdotool key "4"
-  xdotool key "5"
+
+  for i in 2 3 4 5; do
+    xdotool keydown $i
+    sleep $keyrandom
+    xdotool keyup $i
+  done
+
   sleep $timer
+
 done
