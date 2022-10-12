66136a4d14e38a1719f9a280ef73fc1731968b78
Cakenes <jyri.kurtakko@gmail.com>
2022-10-12 14:28:46 +0300

    reverted screenOff

--- screen-off.sh
+++ screen-off.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 for pid in $(pidof -x screen-off.sh); do
-  if [ $pid != $$ ]; then
-    exit 1
-  fi
+    if [ $pid != $$ ]; then
+        exit 1
+    fi
 done
 
 sleep 2
-nvidia-smi -rgc
 xset dpms force off
 
 while :
 do
-  sleep 30
-  pid=$(pidof -x i3lock)
-  if [[ ! -z $pid ]]; then
-    xset dpms force off
-  else
-    exit 1
-  fi
+    sleep 30
+    pid=$(pidof -x i3lock)
+    if [[ ! -z $pid ]]; then
+        xset dpms force off
+    else
+        exit 1
+    fi
 done
