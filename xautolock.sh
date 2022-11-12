cb58208ee67befdb6c35867895e535e95e6f993e
Cakenes <jyri.kurtak@gmail.com>
2022-11-12 19:03:28 +0200

    fixed battery switching with xautolock

--- xautolock.sh
+++ xautolock.sh
@@ -1,34 +1,35 @@
 #!/bin/bash
 
-let type=none
+type=wall
 
 while :
 do 
-    echo $type
     pid=$(pidof -x xautolock)
     battery=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement org.freedesktop.PowerManagement.GetOnBattery | awk NR==2'{print $2}')
     inhibit=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement/Inhibit org.freedesktop.PowerManagement.Inhibit.HasInhibit | awk NR==2'{print $2}')
 
     if [ $battery == "false" ] && [ $type != wall ]; then
+        echo Killing xAutolock, changing to wall power.
         killall xautolock
     elif [ $battery == "true" ] && [ $type != battery ]; then
+        echo Killing xAutolock, changing to battery power.
         killall xautolock
     fi
 
     if [ $inhibit == "false" ] && [ -z $pid ]; then
 	if [ $battery == "false" ]; then
-	    let type=wall
-            echo xautolock ON 15min
+	    type=wall
+            echo xAutolock ON with 15min timer.
             xautolock -time 15 -locker "sh /home/used/.config/scripts/screen-off.sh" &
         else
-            let type=battery
-            echo xautolock ON 5min
+            type=battery
+            echo xAutolock ON with 5min timer.
             xautolock -time 5 -locker "sh /home/used/.config/scripts/screen-off.sh" &
         fi
     fi
 
     if [ $inhibit == "true" ] && [ ! -z $pid ]; then
-        echo xautolock OFF
+        echo Killing xAutolock, inhibiting
         killall xautolock
     fi
 
