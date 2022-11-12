af3e13d8f7a287379ddb09177b4c03dfada3f348
Cakenes <jyri.kurtakko@gmail.com>
2022-11-12 18:47:58 +0200

    added batterymode to xautolock.sh

--- xautolock.sh
+++ xautolock.sh
@@ -2,15 +2,22 @@
 
 while :
 do 
+    battery=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement org.freedesktop.PowerManagement.GetOnBattery | awk NR==2'{print $2}')
     inhibit=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement/Inhibit org.freedesktop.PowerManagement.Inhibit.HasInhibit | awk NR==2'{print $2}')
     pid=$(pidof -x xautolock)
 
-    if [ $inhibit != "true" ] && [ -z $pid ]; then
+    echo $battery
+
+    if [ $inhibit == "false" ] && [ -z $pid ]; then
         echo xautolock ON
-        xautolock -time 15 -locker "sh /home/used/.config/scripts/screen-off.sh" &
+	if [ $battery == "false" ]; then
+            xautolock -time 15 -locker "sh /home/used/.config/scripts/screen-off.sh" &
+        else
+            xautolock -time 5 -locker "sh /home/used/.config/scripts/screen-off.sh" &
+        fi
     fi
 
-    if [ $inhibit != "false" ] && [ ! -z $pid ]; then
+    if [ $inhibit == "true" ] && [ ! -z $pid ]; then
         echo xautolock OFF
         killall xautolock
     fi
