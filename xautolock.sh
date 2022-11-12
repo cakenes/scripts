15472010b4bedf5c9d75e30e67735fac98b6b639
Cakenes <jyri.kurtakko@gmail.com>
2022-11-12 19:01:05 +0200

    added battery detection

--- xautolock.sh
+++ xautolock.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+let type=none
+
 while :
 do 
+    echo $type
+    pid=$(pidof -x xautolock)
     battery=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement org.freedesktop.PowerManagement.GetOnBattery | awk NR==2'{print $2}')
     inhibit=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement/Inhibit org.freedesktop.PowerManagement.Inhibit.HasInhibit | awk NR==2'{print $2}')
-    pid=$(pidof -x xautolock)
 
-    echo $battery
+    if [ $battery == "false" ] && [ $type != wall ]; then
+        killall xautolock
+    elif [ $battery == "true" ] && [ $type != battery ]; then
+        killall xautolock
+    fi
 
     if [ $inhibit == "false" ] && [ -z $pid ]; then
 	if [ $battery == "false" ]; then
+	    let type=wall
             echo xautolock ON 15min
             xautolock -time 15 -locker "sh /home/used/.config/scripts/screen-off.sh" &
         else
+            let type=battery
             echo xautolock ON 5min
             xautolock -time 5 -locker "sh /home/used/.config/scripts/screen-off.sh" &
         fi
