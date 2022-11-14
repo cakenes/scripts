dd6567b4e26319bffa20c3f2af61a9921ad4adfc
Cakenes <jyri.kurtakko@gmail.com>
2022-11-14 17:32:41 +0200

    Removed monitor on check from screen-off.sh

--- screen-off.sh
+++ screen-off.sh
@@ -13,11 +13,10 @@ do
     sleep 5
     pid=$(pidof -x i3lock)
     idle=$(xprintidle)
-    monitor=$(xset q -display :0 | grep 'Monitor is On')
 
     if [[ -z $pid ]]; then
         exit 1
-    elif [[ 10000 -lt $idle ]] && [[ -n $monitor ]]; then
+    elif [[ 10000 -lt $idle ]]; then
         xset dpms force off
     fi
 done
