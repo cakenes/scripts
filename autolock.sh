bd8bdc44ad3914f3831a445170edf8498758e502
cakenes <jyri.kurtakko@gmail.com>
2024-02-07 18:41:42 +0200

    Update autolcok to take in 3 params, fixed functionality

--- autolock.sh
+++ autolock.sh
@@ -5,8 +5,15 @@ if ! [[ -x "$(command -v xprintidle)" ]]; then
   exit 1
 fi
 
-if [[ -z $1 ]] || [[ -z $2 ]]; then
-  echo "Error: parameters not set."
+if ! [[ -x "$(command -v xsecurelock)" ]]; then
+  echo "Error: xsecurelock is not installed."
+  exit 1
+fi
+
+if [[ -z $1 ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
+  echo "Error: set parameters: ac, battery, interval in seconds"
+  echo "       e.g. autolock.sh 600 300 10"
+  echo "       for 10min on ac, 5min on battery, 10sec checking interval" 
   exit 1
 fi
 
@@ -20,8 +27,7 @@ fi
 
 while :
 do
-   sleep 10
-
+   sleep $3
    idle=$(xprintidle)
    pid=$(pgrep xsecurelock)
    inhibit=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement/Inhibit org.freedesktop.PowerManagement.Inhibit.HasInhibit | awk NR==2'{print $2}')
@@ -29,16 +35,18 @@ do
 
    if [[ $inhibit == "true" ]]; then
       idleOffset=$idle
-   elif [[ $inhibit == "false" ]] && [[ -z $pid ]]; then
-      if [[ $idle -gt $(($1 * 60000 + $idleOffset)) && $battery == "true" ]]; then
-	 env XSECURELOCK_BLANK_TIMEOUT=5 XSECURELOCK_AUTH_TIMEOUT=15 XSECURELOCK_SAVER=saver_blank XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 xsecurelock
-      elif [[ $idle -gt $(($2 * 60000)) && $battery == "false" ]]; then
-	 env XSECURELOCK_BLANK_TIMEOUT=5 XSECURELOCK_AUTH_TIMEOUT=15 XSECURELOCK_SAVER=saver_blank XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 xsecurelock
-      fi
    fi
 
    if [[ $idle -lt $idleOffset ]]; then
       idleOffset=0
    fi
 
+   acTarget=$(($1 * 1000 + $idleOffset))
+   batTarget=$(($2 * 1000 + $idleOffset))
+
+   if [[ -z $pid ]]; then
+      if [[ $idle -gt $acTarget && $battery == "false" ]] || [[ $idle -gt $batTarget && $battery == "true" ]]; then
+	 env XSECURELOCK_BLANK_TIMEOUT=5 XSECURELOCK_AUTH_TIMEOUT=15 XSECURELOCK_SAVER=saver_blank XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 xsecurelock
+      fi
+   fi
 done
