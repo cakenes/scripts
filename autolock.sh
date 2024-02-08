d6d85c50535ee9207a03194ee3f37afbc4330291
cakenes <jyri.kurtakko@gmail.com>
2024-02-08 17:14:15 +0200

    Rework autolock

--- autolock.sh
+++ autolock.sh
@@ -12,8 +12,8 @@ fi
 
 if [[ -z $1 ]] || [[ -z $2 ]] || [[ -z $3 ]]; then
   echo "Error: set parameters: ac, battery, interval in seconds"
-  echo "       e.g. autolock.sh 600 300 10"
-  echo "       for 10min on ac, 5min on battery, 10sec checking interval" 
+  echo "       e.g. autolock.sh 600 900 10"
+  echo "       for 10min for lockscreen, 15min for screen off, 10sec checking interval" 
   exit 1
 fi
 
@@ -30,9 +30,13 @@ do
    sleep $3
    idle=$(xprintidle)
    pid=$(pgrep xsecurelock)
-   inhibit=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement/Inhibit org.freedesktop.PowerManagement.Inhibit.HasInhibit | awk NR==2'{print $2}')
-   battery=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement org.freedesktop.PowerManagement.GetOnBattery | awk NR==2'{print $2}')
+   inhibit="false"
+   battery="false"
 
+   if [[ -x "$(command -v xfce4-session)" ]]; then
+      inhibit=$(dbus-send --session --print-reply --dest=org.freedesktop.PowerManagement /org/freedesktop/PowerManagement/Inhibit org.freedesktop.PowerManagement.Inhibit.HasInhibit | awk NR==2'{print $2}')
+   fi   
+      
    if [[ $inhibit == "true" ]]; then
       idleOffset=$idle
    fi
@@ -40,13 +44,17 @@ do
    if [[ $idle -lt $idleOffset ]]; then
       idleOffset=0
    fi
+	
+   lockTarget=$(($1 * 1000 + $idleOffset))
+   offTarget=$(($2 * 1000 + $idleOffset))
 
-   acTarget=$(($1 * 1000 + $idleOffset))
-   batTarget=$(($2 * 1000 + $idleOffset))
-
-   if [[ -z $pid ]]; then
-      if [[ $idle -gt $acTarget && $battery == "false" ]] || [[ $idle -gt $batTarget && $battery == "true" ]]; then
-	 env XSECURELOCK_BLANK_TIMEOUT=5 XSECURELOCK_AUTH_TIMEOUT=15 XSECURELOCK_SAVER=saver_blank XSECURELOCK_DISCARD_FIRST_KEYPRESS=0 xsecurelock
-      fi
+   if [[  -z $pid ]] && [[ $1 -gt 0 ]] && [[ $idle -gt $lockTarget ]]; then
+      xsecurelock
+   fi
+ 
+   if [[ $2 -gt 0 ]] && [[ $idle -gt $offTarget ]]; then
+      xrandr --output DP-2 --off --output DP-0 --off
+   else
+      xrandr --output DP-2 --mode 3440x1440 --rate 75 --output DP-0 --off
    fi
 done
