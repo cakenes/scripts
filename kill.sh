2a9a65ee37c20e345515f86635a100524b2b2408
cakenes <jyri.kurtakko@gmail.com>
2025-02-23 18:04:53 +0200

    Add the isle

--- kill.sh
+++ kill.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-focus=$(xdotool getwindowfocus getwindowname)
-ignore=("Path of Exile" "World of Warcraft" "ARK: Survival Evolved" "Last Epoch")
+focus=$(xdotool getwindowfocus getwindowname | xargs)
+ignore=("Path of Exile" "World of Warcraft" "ARK: Survival Evolved" "Last Epoch" "TheIsle")
 
 for value in "${ignore[@]}"; do
-   if [[ $focus == $value ]]; then
-      exit 1
-   fi
+	value=$(echo "$value" | xargs)
+	if [[ $focus == "$value" ]]; then
+		exit 1
+	fi
 done
 
 wmctrl -c :ACTIVE:
