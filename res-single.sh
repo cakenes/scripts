c9e788eaee7452e1f19aff6f838804eaaf595a2d
cakenes <jyri.kurtakko@gmail.com>
2026-08-17 08:02:05 +0300

    Add resolution scripts

---
+++ res-single.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+swaymsg "output DP-1 disable; output DP-2 res 3440x1440 pos 0 0; output HDMI-A-1 disable"
---
+++ res-triple.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+swaymsg "output DP-1 res 3440x1440 pos 0 0; output DP-2 res 3440x1440 pos 3440 0; output HDMI-A-1 res 3440x1440 pos 3440 0"
